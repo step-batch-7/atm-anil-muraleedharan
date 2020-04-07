745550d3757bdef3523500331d6282246b475338
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-04-07 23:17:19 +0530

    Added get_money and display_notes functions.
    Added a function get_money to split down the amount as per currencies.
    
    Added a function display_notes to display the results of the operation.

---
+++ atm.c
@@ -0,0 +1,20 @@
+#include "atm.h"
+#include<stdio.h>
+
+unsigned int get_money(unsigned short int amount)
+{
+  if(amount>31999)
+  {
+    return 0;
+  }
+  int notes[] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  unsigned int notes_count = 0;
+  for (int index = 0; index < 8; index++)
+  {
+    notes_count = notes_count << 4;
+    notes_count = notes_count | (amount / notes[index]);
+    amount = amount % notes[index];
+  }
+  return notes_count;
+};
+
